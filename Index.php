844e87e1c44cfcb24734782bbaac2302545cdce8
eduardomtzgit <eduardomtzre20@gmail.com>
2023-11-30 18:23:11 -0600

    Implementacion de funcion modificar

--- Index.php
+++ Index.php
@@ -8,17 +8,9 @@
 	<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 	<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 	<style>
-		* {
-			font-size: 16px;
-		}
-
-		h1 {
-			font-size: 25px;
-		}
-
-		p {
-			margin: 0;
-		}
+		* { font-size: 16px; }
+		h1 { font-size: 25px; }
+		p { margin: 0; }
 
 		input[type=number], input[type=text] {
 			width: 70%;
@@ -43,31 +35,31 @@
 			<div align="center" style="display: flex;">
 				<div>
 					<p>Id</p>
-					<input type="number" name="id">
+					<input type="number" name="id" id="inpId">
 				</div>
 				<div>
 					<p>Articulo</p>
-					<input type="text" name="articulo">
+					<input type="text" name="articulo" id="inpArt">
 				</div>
 				<div>
 					<p>Cantidad</p>
-					<input type="number" name="cantidad">
+					<input type="number" name="cantidad" id="inpCant">
 				</div>
 				<div>
 					<p>Precio</p>
-					<input type="number" name="precio">
+					<input type="number" name="precio" id="inpPrec">
 				</div>
 				<div>
 					<p>Descuento</p>
-					<input type="text" name="descuento">
+					<input type="text" name="descuento" id="inpDesc">
 				</div>
 				<div>
 					<p>Utilidad</p>
-					<input type="number" name="utilidad">
+					<input type="number" name="utilidad" id="inpUtil">
 				</div>
 				<div>
 					<p>Fecha</p>
-					<input type="text" name="fecha">
+					<input type="text" name="fecha" id="inpFech">
 				</div>
 			</div>
 			<div align="center" style="display: flex;">
@@ -90,7 +82,7 @@
 	                <th>Fecha</th>
 	            </tr>
 	        </thead>
-	        <tbody>
+	        <tbody id="tbody">
 	        	<?php 
 	        		$archivoLectura = file("productos.txt");
 
@@ -128,12 +120,75 @@
 
 				fwrite($archivo, $productoJSON);
 				fclose($archivo);
+
+				echo "<script>
+						var tab = document.getElementById('tbody');
+						var trNode = document.createElement('tr');
+						var tdId = document.createElement('td');
+	        			var tdArt = document.createElement('td');
+	        			var tdCant = document.createElement('td');
+	        			var tdPre = document.createElement('td');
+	        			var tdDesc = document.createElement('td');
+	        			var tdUtil = document.createElement('td');
+	        			var tdFech = document.createElement('td');
+
+						tdId.innerText = '$id'
+	        			tdArt.innerText = '$articulo'
+	        			tdCant.innerText = '$cantidad'
+	        			tdPre.innerText = '$$precio'
+	        			tdDesc.innerText = '$descuento%'
+	        			tdUtil.innerText = '$$utilidad'
+	        			tdFech.innerText = '$fecha'
+
+						trNode.appendChild(tdId);
+						trNode.appendChild(tdArt);
+						trNode.appendChild(tdCant);
+						trNode.appendChild(tdPre);
+						trNode.appendChild(tdDesc);
+						trNode.appendChild(tdUtil);
+						trNode.appendChild(tdFech);
+	        			tab.appendChild(trNode);
+					</script>";
+			} else if(isset($_POST['modificar'])) {
+				$archivoLecturaMod = file("productos.txt");
+				$fileJsonUpd = "";
+				$fileUpd = "";
+
+				foreach($archivoLecturaMod as $line) {
+					$prodts = json_decode($line);
+
+					if($prodts->id != $id) {
+	    				$fileJsonUpd .= '{"id":"'.$prodts->id.'","articulo":"'.$prodts->articulo.'","cantidad":"'.$prodts->cantidad.'","precio":"'.$prodts->precio.'","descuento":"'.$prodts->descuento.'","utilidad":"'.$prodts->utilidad.'","fecha":"'.$prodts->fecha.'"}'.PHP_EOL;
+	    			} else {
+	    				$fileUpd .= '{"id":"'.$id.'","articulo":"'.$articulo.'","cantidad":"'.$cantidad.'","precio":"'.$precio.'","descuento":"'.$descuento.'","utilidad":"'.$utilidad.'","fecha":"'.$fecha.'"}'.PHP_EOL;
+	    			}
+				}
+
+				$fileWrite = fopen("productos.txt", "w");
+
+				fwrite($fileWrite, $fileJsonUpd);
+				fwrite($fileWrite, $fileUpd);
+				fclose($fileWrite);
 			}
 		}
-
 	?>
 	<script>
-		new DataTable('#example');
+		var table = new DataTable('#example');
+
+		table.on("click", "tbody tr", function () {
+    		let data = table.row(this).data();
+    		let nPre = data[3].replace("$", "");
+    		let nDes = data[4].replace("%", "");
+    		let nUtil = data[5].replace("$", "");
+
+    		$('#inpId').val(data[0]);
+    		$('#inpArt').val(data[1]);
+    		$('#inpCant').val(data[2]);
+    		$('#inpPrec').val(nPre);
+    		$('#inpDesc').val(nDes);
+    		$('#inpUtil').val(nUtil);
+    		$('#inpFech').val(data[6]);
+    	})
 	</script>
 </body>
 </html>
